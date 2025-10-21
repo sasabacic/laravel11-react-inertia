a375feea64a176114d0c954198dde7cda08f9677
sasabacic <salezd@yahoo.com>
2025-10-21 21:34:09 +0200

    Updated Pagination with template literals for better readibilty

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,20 +1,30 @@
 import { Link } from "@inertiajs/react";
 
-export default function Pagination({links}){
-    return(
-        <nav className="text-center mt-4">
-            {links.map(link => (
-                <Link
-                preserveScroll
-                href={link.url || ""}
-                key={link.label}
-                className={
-                    "inline-block py-2 px-3 rounded-lg text-gray-200 text-xs " +
-                    (link.active ? "bg-gray-950 " : " " ) +
-                    (!link.url ? "!text-gray-500 cursor-not-allowed " : "hover:bg-gray-950")
-                }
-                dangerouslySetInnerHTML={{ __html: link.label}}></Link>
-            ))}
-        </nav>
-    )
+export default function Pagination({ links }) {
+  if (!links.length) return null; // no pagination if only one page
+
+  return (
+    <nav className="text-center mt-4 space-x-1">
+      {links.map((link, id) => {
+        const isDisabled = !link.url;
+        const isActive = link.active;
+
+        const classes = `
+          inline-block py-2 px-3 rounded-lg text-xs
+          ${isActive ? "bg-gray-950 text-white" : "text-gray-200"}
+          ${isDisabled ? "!text-gray-500 cursor-not-allowed" : "hover:bg-gray-950"}
+        `;
+
+        return (
+          <Link
+            key={id}
+            href={link.url || ""}
+            preserveScroll
+            className={classes}
+            dangerouslySetInnerHTML={{ __html: link.label }}
+          />
+        );
+      })}
+    </nav>
+  );
 }
